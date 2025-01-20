2493a01dbf999036c7f078fa66013d163dd0b464
ankushkumar <1208ankush@gmail.com>
2025-01-20 15:14:56 +0530

    Removes socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,31 +8,10 @@ import { Summary } from "./components/Summary";
 import { Skills } from "./components/Skills";
 import { Header } from "./components/Header";
 import { ContactForm } from "./components/ContactForm";
-import { AboutMe } from "./components/AboutMe";
 
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} - Portfolio`,
   description: RESUME_DATA.about,
-  openGraph: {
-    title: `${RESUME_DATA.name} - Portfolio`,
-    description: RESUME_DATA.about,
-    type: "profile",
-    locale: "en_US",
-    images: [
-      {
-        url: "https://cv.jarocki.me/opengraph-image",
-        width: 1200,
-        height: 630,
-        alt: `${RESUME_DATA.name}'s profile picture`,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: `${RESUME_DATA.name} - Portfolio`,
-    description: RESUME_DATA.about,
-    images: ["https://cv.jarocki.me/opengraph-image"],
-  },
 };
 
 /**
